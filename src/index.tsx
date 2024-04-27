aff7ff36ba29e4edbde2fdc407f4d528f3dce868
sehwan24 <1224ssanta@naver.com>
2024-04-27 21:47:09 +0900

    개발중..

--- src/index.tsx
+++ src/index.tsx
@@ -10,13 +10,9 @@ interface Room {
     roomName: string;
 }
 
-// 메시지의 형식을 정의합니다. 예를 들어, JSON 형식을 가정합니다.
 interface ChatMessage {
-    // 메시지의 속성에 따라 타입을 정의합니다.
-    // 이 예시에서는 메시지 텍스트만 고려합니다.
-    type: 'ENTER' | 'TALK',
-    runningType: 'RUN' | 'STOP',
-    count : number,
+    type: 'ENTER' | 'TALK' | 'VIDEO',
+    runningType: 'RUN' | 'STOP' | 'NO',
     roomId : string,
     sender : string,
     message: string;
@@ -29,9 +25,11 @@ function Example() {
     const [roomName, setRoomName] = useState("");
     const [messages, setMessages] = useState<string[]>([]);
     const [existingRooms, setExistingRooms] = useState<Room[]>([]);
-    const [count] = useState<number>(0);
+    const [playerReady, setPlayerReady] = useState(false);  // 플레이어 준비 상태
+
 
     useEffect(() => {
+        sessionStorage.setItem('currentState', 'STOP');
         // WebSocket을 사용하여 STOMP 클라이언트 생성
         const client = Stomp.client('ws://localhost:8080/ws');
         // STOMP 클라이언트에 대한 설정
@@ -57,50 +55,68 @@ function Example() {
 
 
 
-    const joinRoom = (roomId: string) => {
+    const joinRoom = (roomId: string) => {  //채팅방 입장 시
         console.log(roomId);
+
+        sessionStorage.setItem('roomId', roomId);
 
         if (stompClient) {
             // 채팅방에 입장할 때 해당 채팅방의 메시지를 구독
+            const messageObject = {
+                type: "ENTER",
+                runningType: "NO",
+                roomId: sessionStorage.getItem("roomId"),
+                sender: client.clientId,
+                message: "entered room"
+            };
+            console.log(client.clientId);
+            console.log("run2")
+            // STOMP 클라이언트를 통해 메시지 전송
+            stompClient.publish({ destination: '/pub/chatting/message', body: JSON.stringify(messageObject) });
             stompClient.subscribe(`/sub/chatting/room/${roomId}`, handleStompMessage);
         }
     }
 
     // STOMP 클라이언트가 연결되면 실행될 함수
     const handleStompMessage = (message: Message) => {
-        // Message.body는 문자열 형태이므로 JSON 파싱이 필요합니다.
         const body: ChatMessage = JSON.parse(message.body);
-        // 받은 메시지를 적절히 처리합니다.
-        console.log('Received message:', body.message);
-        if (body.type == 'TALK' && body.sender != client.clientId) {
+        if (player) {
+            player.seekTo(body.message, true);
+        } else {
+            console.error('YouTube 플레이어가 초기화되지 않았습니다');
+        }
+        if (body.type === 'VIDEO' && body.sender !== client.clientId) {
             player.seekTo(body.message, 1);
-            if (body.runningType == "STOP") {
+            if (body.runningType === "STOP" && sessionStorage.getItem("currentState") === "RUN") {
                 player.pauseVideo();
-                console.log(client.clientId);
-                console.log("stop");
-            } else if (body.runningType == "RUN") {
+                sessionStorage.setItem('currentState', "STOP");
+            } else if (body.runningType === "RUN" && sessionStorage.getItem("currentState") === "STOP") {
                 player.playVideo();
-                console.log(client.clientId);
-                console.log("run");
+                sessionStorage.setItem('currentState', "RUN");
             }
 
+        }
+        if (body.type === 'TALK' && body.sender !== client.clientId) {
+            if (body.runningType === "NO") {
+                setVideoId(body.message);
+            }
         }
     };
 
 
     const onPlayerStateChange: YouTubeProps['onStateChange'] = (event) => {
-        if (event.data === 1) {
+        if (event.data === 1 && sessionStorage.getItem('currentState') !== "RUN") {
             // 영상이 재생 중일 때
+            sessionStorage.setItem('currentState', "RUN");
             const currentTime = player.getCurrentTime(); // 현재 재생 위치를 가져옴 (초 단위)
             console.log('현재 재생 위치:', currentTime);
 
-            // 서버로 현재 재생 위치를 보냄 (fetch 등의 방법을 사용할 수 있음)
+            // 서버로 현재 재생 위치를 보냄
             if (stompClient) {
                 const messageObject = {
-                    type: "TALK",
+                    type: "VIDEO",
                     runningType: "RUN",
-                    count: 0,
-                    roomId: "67be7fb6-3040-475e-aa83-676d0922e5fe",
+                    roomId: sessionStorage.getItem("roomId"),
                     sender: client.clientId,
                     message: currentTime
                 };
@@ -110,7 +126,8 @@ function Example() {
                 stompClient.publish({ destination: '/pub/chatting/message', body: JSON.stringify(messageObject) });
             }
 
-        } else if (event.data === 2) {
+        } else if (event.data === 2 && sessionStorage.getItem('currentState') !== "STOP") {
+            sessionStorage.setItem('currentState', "STOP");
             // 영상이 일시 정지되었을 때
             const currentTime = player.getCurrentTime(); // 현재 재생 위치를 가져옴 (초 단위)
             console.log('일시 정지된 시간:', currentTime);
@@ -118,10 +135,9 @@ function Example() {
             // 서버로 현재 재생 위치를 보냄 (fetch 등의 방법을 사용할 수 있음)
             if (stompClient) {
                 const messageObject = {
-                    type: "TALK",
+                    type: "VIDEO",
                     runningType: "STOP",
-                    count: 0,
-                    roomId: "67be7fb6-3040-475e-aa83-676d0922e5fe",
+                    roomId: sessionStorage.getItem("roomId"),
                     sender: client.clientId,
                     message: currentTime
                 };
@@ -134,10 +150,12 @@ function Example() {
     }
 
 
+
     const onPlayerReady: YouTubeProps['onReady'] = (event) => {
         setPlayer(event.target);
         event.target.pauseVideo();
     }
+
 
     const handleButtonClick2 = () => {
         fetch('http://localhost:8080/chatting/rooms')
@@ -184,6 +202,35 @@ function Example() {
         setRoomName(event.target.value);
     }
 
+    const [videoId, setVideoId] = useState('2g811Eo7K8U'); // 기본 YouTube 비디오 ID
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    }
+
+    const updateVideoId = () => {
+        const videoIdFromURI =
+            inputValue.split('?v=').length > 1 ? inputValue.split('?v=')[1].split('&')[0] : false;
+        if (videoIdFromURI) {
+            setVideoId(videoIdFromURI);
+        }
+
+        if(videoIdFromURI) {
+            if (stompClient) {
+                const messageObject = {
+                    type: "TALK",
+                    runningType: "NO",
+                    roomId: sessionStorage.getItem("roomId"),
+                    sender: client.clientId,
+                    message: videoIdFromURI
+                };
+                // STOMP 클라이언트를 통해 메시지 전송
+                stompClient.publish({ destination: '/pub/chatting/message', body: JSON.stringify(messageObject) });
+            }
+        }
+    }
+
     const opts: YouTubeProps['opts'] = {
         height: '390',
         width: '640',
@@ -195,21 +242,34 @@ function Example() {
     return (
         <div>
             <YouTube
-                videoId="2g811Eo7K8U"
+                videoId={videoId}
                 opts={opts}
                 onReady={onPlayerReady}
                 onStateChange={onPlayerStateChange}
             />
             <input
                 type="text"
-                value={roomName}
-                onChange={handleRoomNameChange}
-                placeholder="방 이름을 입력하세요"
-                style={{ margin: '10px 0' }}
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="YouTube 비디오 링크를 입력하세요"
+                style={{ width: '30%', marginRight: '10px' }}
             />
-            <button onClick={handleButtonClick}>방 만들기</button>
+            <button onClick={updateVideoId}>시청하기</button>
 
-            <button onClick={handleButtonClick2}>방 목록 조회</button>
+            <div>
+                <input
+                    type="text"
+                    value={roomName}
+                    onChange={handleRoomNameChange}
+                    placeholder="방 이름을 입력하세요"
+                    style={{ width: '30%', marginRight: '10px' }}
+                />
+                <button
+                    style={{ marginRight: '10px' }}
+                    onClick={handleButtonClick}>방 만들기 </button>
+
+                <button onClick={handleButtonClick2}>방 목록 조회</button>
+            </div>
 
             <div>
                 <h2>현재 있는 방</h2>
