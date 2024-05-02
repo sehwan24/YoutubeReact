4c47180ad2404ccafe0290f804d77bb68d5a1092
sehwan24 <1224ssanta@naver.com>
2024-05-02 16:47:01 +0900

    링크 선택까지 완료

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import ReactDOM from 'react-dom';
-import YouTube, { YouTubeProps } from 'react-youtube';
+import YouTube, {YouTubeEvent, YouTubeProps} from 'react-youtube';
 import { Client, Stomp } from '@stomp/stompjs'; // STOMP 클라이언트 및 WebSocket 가져오기
 import { Message } from '@stomp/stompjs';
 import { v4 as uuidv4 } from 'uuid';
+
 
 interface Room {
     roomId: string;
@@ -26,9 +27,36 @@ function Example() {
     const [messages, setMessages] = useState<string[]>([]);
     const [existingRooms, setExistingRooms] = useState<Room[]>([]);
     const [playerReady, setPlayerReady] = useState(false);  // 플레이어 준비 상태
+    const [videoId, setVideoId] = useState('2g811Eo7K8U');
+    const [inputValue, setInputValue] = useState('');
 
 
     useEffect(() => {
+        console.log('plz use Effect');
+        console.log(player);
+        console.log(playerReady);
+        if (player && playerReady) {
+            console.log("Ohhhhhhh");
+            try {
+                const roomId = sessionStorage.getItem("roomId");
+                if (roomId) {
+                    joinRoom(roomId);
+                } else {
+                    console.error("Room ID is not available.");
+                }
+                sessionStorage.setItem('currentState', "RUN");
+                console.log('세션 룸id' + sessionStorage.getItem("roomId"));
+                player.loadVideoById(videoId, 0, 'large');
+            } catch (error) {
+                console.error("Error loading video:", error);
+            }
+        }
+    }, [videoId, player, playerReady]);
+
+
+
+    useEffect(() => {
+        console.log('use Effect !!!!!!!!!!!!!!!!!!');
         sessionStorage.setItem('currentState', 'STOP');
         // WebSocket을 사용하여 STOMP 클라이언트 생성
         const client = Stomp.client('ws://localhost:8080/ws');
@@ -55,8 +83,42 @@ function Example() {
 
 
 
+
+
+    // STOMP 클라이언트가 연결되면 실행될 함수
+    const handleStompMessage = (message: Message) => {
+        const body: ChatMessage = JSON.parse(message.body);
+        if (player) {
+            player.seekTo(body.message, true);
+        } else {
+            console.error('YouTube 플레이어가 초기화되지 않았습니다');
+        }
+        console.log('1');
+        if (body.type === 'VIDEO' && body.sender !== client.clientId) {
+            console.log('2');
+            player.seekTo(body.message, 1);
+            if (body.runningType === "STOP" && sessionStorage.getItem("currentState") === "RUN") {
+                console.log('3')
+                player.pauseVideo();
+                sessionStorage.setItem('currentState', "STOP");
+            } else if (body.runningType === "RUN" && sessionStorage.getItem("currentState") === "STOP") {
+                console.log('4');
+                player.playVideo();
+                sessionStorage.setItem('currentState', "RUN");
+            }
+
+        }
+        if (body.type === 'TALK' && body.sender !== client.clientId) {
+            if (body.runningType === "NO") {
+                setVideoId(body.message);
+                sessionStorage.setItem('currentState', "STOP");
+            }
+        }
+    };
+
     const joinRoom = (roomId: string) => {  //채팅방 입장 시
         console.log(roomId);
+        console.log('join ! ! ! ! ! ! ! ');
 
         sessionStorage.setItem('roomId', roomId);
 
@@ -75,36 +137,14 @@ function Example() {
             stompClient.publish({ destination: '/pub/chatting/message', body: JSON.stringify(messageObject) });
             stompClient.subscribe(`/sub/chatting/room/${roomId}`, handleStompMessage);
         }
-    }
-
-    // STOMP 클라이언트가 연결되면 실행될 함수
-    const handleStompMessage = (message: Message) => {
-        const body: ChatMessage = JSON.parse(message.body);
-        if (player) {
-            player.seekTo(body.message, true);
-        } else {
-            console.error('YouTube 플레이어가 초기화되지 않았습니다');
-        }
-        if (body.type === 'VIDEO' && body.sender !== client.clientId) {
-            player.seekTo(body.message, 1);
-            if (body.runningType === "STOP" && sessionStorage.getItem("currentState") === "RUN") {
-                player.pauseVideo();
-                sessionStorage.setItem('currentState', "STOP");
-            } else if (body.runningType === "RUN" && sessionStorage.getItem("currentState") === "STOP") {
-                player.playVideo();
-                sessionStorage.setItem('currentState', "RUN");
-            }
-
-        }
-        if (body.type === 'TALK' && body.sender !== client.clientId) {
-            if (body.runningType === "NO") {
-                setVideoId(body.message);
-            }
-        }
     };
 
 
     const onPlayerStateChange: YouTubeProps['onStateChange'] = (event) => {
+        if (!player || !playerReady) {
+            console.log("Player is not ready or not available.");
+            return;
+        }
         if (event.data === 1 && sessionStorage.getItem('currentState') !== "RUN") {
             // 영상이 재생 중일 때
             sessionStorage.setItem('currentState', "RUN");
@@ -147,14 +187,21 @@ function Example() {
                 stompClient.publish({ destination: '/pub/chatting/message', body: JSON.stringify(messageObject) });
             }
         }
-    }
+    };
 
 
 
     const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+        console.log("Player is ready:", event.target);  // 로깅 추가
+        if (!event.target) {
+            console.error("YouTube Player is not initialized properly.");
+            return;
+        }
         setPlayer(event.target);
+        setPlayerReady(true);
         event.target.pauseVideo();
-    }
+    };
+
 
 
     const handleButtonClick2 = () => {
@@ -202,17 +249,16 @@ function Example() {
         setRoomName(event.target.value);
     }
 
-    const [videoId, setVideoId] = useState('2g811Eo7K8U'); // 기본 YouTube 비디오 ID
-    const [inputValue, setInputValue] = useState('');
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(event.target.value);
     }
 
     const updateVideoId = () => {
+        //player.loadVideoByUrl(inputValue);
         const videoIdFromURI =
             inputValue.split('?v=').length > 1 ? inputValue.split('?v=')[1].split('&')[0] : false;
-        if (videoIdFromURI) {
+        if (videoIdFromURI && playerReady) {
             setVideoId(videoIdFromURI);
         }
 
@@ -229,13 +275,15 @@ function Example() {
                 stompClient.publish({ destination: '/pub/chatting/message', body: JSON.stringify(messageObject) });
             }
         }
-    }
+    };
+
 
     const opts: YouTubeProps['opts'] = {
         height: '390',
         width: '640',
         playerVars: {
             autoplay: 1,
+            origin: window.location.origin,
         },
     };
 
